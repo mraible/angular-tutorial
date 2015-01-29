56be9decd0242dd60f06ef7232db723d6595ed0c
mraible <matt@raibledesigns.com>
2015-01-29 10:58:44 -0700

    Add an Edit Feature

--- app/search/mock-api.js
+++ app/search/mock-api.js
@@ -60,6 +60,35 @@ angular.module('myApp')
                 return list;
             }
         };
+
+        this.find = function (id) {
+            // find the game that matches that id
+            var list = $.grep(this.getData(), function (element, index) {
+                return (element.id == id);
+            });
+            if (list.length === 0) {
+                return {};
+            }
+            // even if list contains multiple items, just return first one
+            return list[0];
+        };
+
+        this.update = function (id, dataItem) {
+            // find the game that matches that id
+            var people = this.getData();
+            var match = null;
+            for (var i = 0; i < people.length; i++) {
+                if (people[i].id == id) {
+                    match = people[i];
+                    break;
+                }
+            }
+            if (!angular.isObject(match)) {
+                return {};
+            }
+            angular.extend(match, dataItem);
+            return match;
+        };
     })
     .run(function ($httpBackend, ServerDataModel) {
 
@@ -74,4 +103,32 @@ angular.module('myApp')
 
         $httpBackend.whenGET(/search\/index.html/).passThrough();
         $httpBackend.whenGET(/view/).passThrough();
+
+        $httpBackend.whenGET(/\/search/).respond(function (method, url, data) {
+            var results = ServerDataModel.search("");
+
+            return [200, results];
+        });
+
+        $httpBackend.whenGET(/\/edit\/\d+/).respond(function (method, url, data) {
+            // parse the matching URL to pull out the id (/edit/:id)
+            var id = url.split('/')[2];
+
+            var results = ServerDataModel.find(id);
+
+            return [200, results, {Location: '/edit/' + id}];
+        });
+
+        $httpBackend.whenPOST(/\/edit\/\d+/).respond(function(method, url, data) {
+            var params = angular.fromJson(data);
+
+            // parse the matching URL to pull out the id (/edit/:id)
+            var id = url.split('/')[2];
+
+            var person = ServerDataModel.update(id, params);
+
+            return [201, person, { Location: '/edit/' + id }];
+        });
+
+        $httpBackend.whenGET(/search\/edit.html/).passThrough();
     });
--- app/search/search.js
+++ app/search/search.js
@@ -6,21 +6,58 @@ angular.module('myApp.search', ['ngRoute'])
                 templateUrl: 'search/index.html',
                 controller: 'SearchController'
             })
+            .when('/search/:term', {
+                templateUrl: 'search/index.html',
+                controller: 'SearchController'
+            })
+            .when('/edit/:id', {
+                templateUrl: 'search/edit.html',
+                controller: 'EditController'
+            });
     }])
 
-    .controller('SearchController', function ($scope, SearchService) {
+    .controller('SearchController', function ($scope, $location, $routeParams, SearchService) {
+        if ($routeParams.term) {
+            SearchService.query($routeParams.term).then(function (response) {
+                $scope.term = $routeParams.term;
+                $scope.searchResults = response.data;
+            });
+        }
+
         $scope.search = function () {
             console.log("Search term is: " + $scope.term);
             SearchService.query($scope.term).then(function (response) {
                 $scope.searchResults = response.data;
             });
         };
+
+        $scope.edit = function (person) {
+            $location.path("/edit/" + person.id);
+        }
+    })
+
+    .controller('EditController', function ($scope, $location, $routeParams, SearchService) {
+        SearchService.fetch($routeParams.id).then(function (response) {
+            $scope.person = response.data;
+        });
+
+        $scope.save = function() {
+            SearchService.save($scope.person).then(function(response) {
+                $location.path("/search/" + $scope.person.name);
+            });
+        }
     })
 
     .factory('SearchService', function ($http) {
         var service = {
             query: function (term) {
                 return $http.get('/search/' + term);
+            },
+            fetch: function (id) {
+                return $http.get('/edit/' + id);
+            },
+            save: function(data) {
+                return $http.post('/edit/' + data.id, data);
             }
         };
         return service;
