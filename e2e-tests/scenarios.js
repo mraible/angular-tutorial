12e37eb7ad8cde82c6ec92c8562700a7fb6952cc
mraible <matt@raibledesigns.com>
2015-02-02 10:05:08 -0700

    Integration Tests

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -39,4 +39,62 @@ describe('my app', function() {
     });
 
   });
+    
+  describe('search', function() {
+    var searchTerm = element(by.model('term'));
+    var searchButton = element(by.id('search'));
+
+    beforeEach(function() {
+      browser.get('index.html#/search');
+    });
+
+    it('should allow searching at /search', function() {
+      searchTerm.sendKeys("M");
+      searchButton.click().then(function() {
+        expect(element.all(by.repeater('person in searchResults')).count()).toEqual(3);
+      });
+    });
+  });
+
+  describe('edit person', function() {
+    var name = element(by.model('person.name'));
+    var street = element(by.model('person.address.street'));
+    var city = element(by.model('person.address.city'));
+
+    beforeEach(function() {
+      browser.get('index.html#/edit/1');
+    });
+
+    it('should allow viewing a person', function() {
+      // getText() doesn't work with input elements, see the following for more information:
+      // https://github.com/angular/protractor/blob/master/docs/faq.md#the-result-of-gettext-from-an-input-element-is-always-empty
+      expect(name.getAttribute('value')).toEqual("Peyton Manning");
+      expect(street.getAttribute('value')).toEqual("1234 Main Street");
+      expect(city.getAttribute('value')).toEqual("Greenwood Village");
+    });
+  });
+
+  describe('save person', function() {
+    var name = element(by.model('person.name'));
+    var save = element(by.id('save'));
+
+    beforeEach(function() {
+      browser.get('index.html#/edit/1');
+    });
+
+    it('should allow updating a name', function() {
+      name.sendKeys(" Updated");
+      save.click().then(function() {
+        // verify url set back to search results
+        browser.driver.wait(function() {
+          return browser.driver.getCurrentUrl().then(function(url) {
+            expect(url).toContain('/search/Peyton%20Manning%20Updated');
+            return url;
+          });
+        });
+        // verify one element matched this change
+        expect(element.all(by.repeater('person in searchResults')).count()).toEqual(1);
+      });
+    });
+  });
 });
